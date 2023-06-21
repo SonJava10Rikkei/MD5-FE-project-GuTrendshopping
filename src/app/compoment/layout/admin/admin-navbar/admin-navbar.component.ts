2bcdfd98d2664f3e2b347394b50d4131945ecc70
SonJava10Rikkei <sonjapan7@gmail.com>
2023-06-21 16:09:50 +0700

    trang admin js custom

--- src/app/compoment/layout/admin/admin-navbar/admin-navbar.component.ts
+++ src/app/compoment/layout/admin/admin-navbar/admin-navbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {adminSidebar} from "../../../customs/js/admin-sidebar";
 
 @Component({
   selector: 'app-admin-navbar',
   templateUrl: './admin-navbar.component.html',
   styleUrls: ['./admin-navbar.component.scss']
 })
-export class AdminNavbarComponent {
+export class AdminNavbarComponent implements OnInit{
+  ngOnInit(): void {
+
+    adminSidebar()
+  }
 
 }
+
